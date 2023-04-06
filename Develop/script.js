3f6d10877a2b738adee383cd8934f769c7e97d6d
Beenz <CH.SABEEN@GMAIL.COM>
2023-04-05 22:32:32 -0700

    set and get from local storage function added

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,4 @@
-//Add a listener for click events on the save button. This code should
-// use the id in the container time-block as a key to save the user input in
-// local storage. HINT: What does `this` reference in the click listener
-// function? How can DOM traversal be used to get the "hour-x" id of the
-// time-block containing the button that was clicked? How might the id be
-// useful when saving the description in local storage?
-//wrap code in jquery ready function
+//wrap in jquery ready function
 $(document).ready(function () {
   // today's date from day.js
   var todayDate = dayjs().format("dddd, MMM YYYY");
