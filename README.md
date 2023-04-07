6f665c0cdb7c11f0d13943f219be9c42db094942
Beenz <CH.SABEEN@GMAIL.COM>
2023-04-06 19:11:43 -0700

    files rearranged for github deployment

---
+++ README.md
@@ -0,0 +1,55 @@
+# 05 Third-Party APIs: Work Day Scheduler
+
+## Overview
+
+A simple daily planner application that allows a user to save events for each hour of the day. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery. It also makes use of Day.js for day and time.
+
+## Technologies Used
+
+html
+css
+jquery
+bootstrap
+day.js
+
+## Code Summary
+
+Following is the driving function. CurrentTime was set to a 24 hours format using Day.js and blockTime represents the time of each hour in the planner. Classes of past,present and future are added or removed depending if blockTime on the planner is greater, equalor less than the currentTime.
+Not shown here-each timeblock is color coded using css style to indicate whether it is in the past, present, or future. Also, the user entry is saved to the local storage.
+
+function timeKeeper() {
+var currentTime = dayjs().hour();
+
+    console.log(currentTime);
+
+    $(.saveBtn).each(function () {
+      var blockTime = parseInt($(this).attr("id"))
+
+      if (blockTime < currentTime) {
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+        $(this).addClass("past");
+      } else if (blockTime === currentTime) {
+        $(this).removeClass("future");
+        $(this).addClass("present");
+        $(this).removeClass("past");
+      } else if (blockTime > currentTime) {
+        $(this).addClass("future");
+        $(this).removeClass("present");
+        $(this).removeClass("past");
+      }
+    });}
+
+timeKeeper();
+
+## Demo
+
+<img src="demo.gif>" alt="Project GIF" width="500">
+
+## Acknowledgement:
+
+UCBerkely Web Development Bootcamp Team for providing the starter code.
+
+## code by
+
+Sabeen Chaudhry
---
+++ script.js
@@ -0,0 +1,56 @@
+//wrap in jquery ready function
+$(document).ready(function () {
+  // today's date from day.js
+  var todayDate = dayjs().format("dddd, MMM YYYY");
+  $("#currentDay").text(todayDate);
+
+  var time = dayjs().format("hh:mm:ss");
+  $("#currentDay").text(time);
+
+  function timeKeeper() {
+    //current time set using dayjs
+    var currentTime = dayjs().hour();
+
+    console.log(currentTime);
+
+    //For each time block check if blockTime=currenttime to add/remove classes for background color
+
+    $(".time-block").each(function () {
+      //id had to be changed to an integer
+      var blockTime = parseInt($(this).attr("id"));
+
+      // getting the values of the textarea elements saved in the local storage
+      var textBlock = localStorage.getItem(blockTime);
+      console.log(textBlock);
+
+      $(this).children(".description").val(textBlock);
+
+      if (blockTime < currentTime) {
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+        $(this).addClass("past");
+      } else if (blockTime === currentTime) {
+        $(this).removeClass("future");
+        $(this).addClass("present");
+        $(this).removeClass("past");
+      } else if (blockTime > currentTime) {
+        $(this).addClass("future");
+        $(this).removeClass("present");
+        $(this).removeClass("past");
+      }
+    });
+  }
+
+  timeKeeper();
+});
+
+//Adding a listener for click on the save button and saving to local storage using 'this' and dom traversing
+$(".saveBtn").on("click", function () {
+  var timeSlot = $(this).parent().attr("id");
+  var userText = $(this).siblings(".description").val();
+
+  console.log(timeSlot);
+  console.log(userText);
+
+  localStorage.setItem(timeSlot, userText);
+});
